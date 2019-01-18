8f7c3b90b9261e576665c4173156ec6082090366
Stone Tickle <lattis@mochiro.moe>
2019-01-18 15:57:54 -0600

    add a probably useless test

--- spec/packserv_spec.rb
+++ spec/packserv_spec.rb
@@ -1,21 +1,22 @@
 RSpec.describe PackServ do
+  before(:all) { start_server }
+  after(:all) { stop_server }
+
   it 'has a version number' do
     expect(PackServ::VERSION).not_to be nil
   end
 
   context 'the client dies' do
+    before(:all) { connect_client }
+
+    it 'wont affect the server' do
+      disconnect_client
+    end
   end
 
   context 'all together' do
-    before(:all) do
-      start_server
-      connect_client
-    end
-
-    after(:all) do
-      disconnect_client
-      stop_server
-    end
+    before(:all) { connect_client }
+    after(:all) { disconnect_client }
 
     it 'can communicate' do
       expect(client.send('hello')).to eq 'hi'
