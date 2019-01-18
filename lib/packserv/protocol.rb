c1fb257d083172151daf58a1e09374ba8a0fcd20
Stone Tickle <lattis@mochiro.moe>
2019-01-18 14:25:05 -0600

    begin adding protocol module

---
+++ lib/packserv/protocol.rb
@@ -0,0 +1,21 @@
+module PackServ
+  module DefaultProtocol
+    HEADER_LENGTH = 8
+    HEADER_FORMAT = '%08x'.freeze
+
+    module_function
+
+    def create(obj, type, id = nil)
+      {
+        'ver'     => PackServ::VERSION,
+        'id'      => id || obj.object_id,
+        'type'    => type,
+        'payload' => obj
+      }
+    end
+
+    def extract(msg)
+      msg['payload']
+    end
+  end
+end
