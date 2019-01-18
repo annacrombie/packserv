d6602495c90696c25d597f0fb6653bcb99dba077
Stone Tickle <lattis@mochiro.moe>
2019-01-18 14:38:26 -0600

    remove messages from spec

--- spec/packserv_spec.rb
+++ spec/packserv_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe PackServ do
   context 'all together' do
     before(:all) do
       @server = PackServ.serve(12345)
-      puts "started server"
 
       @server.handler = lambda do |message|
         case message
@@ -18,16 +17,11 @@ RSpec.describe PackServ do
       end
 
       @client = PackServ.connect('localhost', 12345)
-      puts "connected"
     end
 
     after(:all) do
-      puts "disconnecting"
       @client.disconnect
-      puts "done"
-      puts "stopping server"
       @server.stop
-      puts "done"
     end
 
     it 'can communicate' do
