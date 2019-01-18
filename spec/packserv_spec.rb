363591030734d8d8166e145bbce5e1c985328599
Stone Tickle <lattis@mochiro.moe>
2019-01-18 13:38:57 -0600

    write a spec

--- spec/packserv_spec.rb
+++ spec/packserv_spec.rb
@@ -2,4 +2,24 @@ RSpec.describe Packserv do
   it 'has a version number' do
     expect(Packserv::VERSION).not_to be nil
   end
+
+  context 'all together' do
+    server = PackServ.server(12345)
+
+    server.handler = lambda do |message|
+      case message
+      when 'hello'
+        'hi'
+      when 'goodbye'
+        'see ya'
+      end
+    end
+
+    client = PackServ.client('localhost', 12345)
+
+    it 'can communicate' do
+      expect(client.send('hello')).to eq('hi')
+      expect(client.send('goodbye')).to eq('see ya')
+    end
+  end
 end
