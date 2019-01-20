a207f001871d2eb19b96d81186d1e03a2ca8d38f
Stone Tickle <lattis@mochiro.moe>
2019-01-20 11:06:15 -0600

    more exceptions

--- lib/packserv/exceptions.rb
+++ lib/packserv/exceptions.rb
@@ -1,4 +1,15 @@
 module PackServ
   module Exceptions
+    class InvalidId < StandardError
+      def initialize(id)
+        super("no queue is open for message id #{id}")
+      end
+    end
+
+    class InvalidMessage < StandardError
+      def initialize(msg)
+        super(msg)
+      end
+    end
   end
 end
